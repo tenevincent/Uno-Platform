ba1f8f62e58374a1b911255837152ad1597e3b6c
Tene Kengne Vincent De Paul <vincent.tene@gmail.com>
2021-08-04 18:05:23 +0200

    nw project added

---
+++ Pluralsight/UnoControlsApp/UnoControlsApp/UnoControlsApp.Skia.Tizen/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tizen.Applications;
+using Uno.UI.Runtime.Skia;
+
+namespace UnoControlsApp.Skia.Tizen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new TizenHost(() => new UnoControlsApp.App(), args);
+            host.Run();
+        }
+    }
+}
---
+++ Pluralsight/UnoControlsApp/UnoControlsApp/UnoControlsApp.Skia.Wpf/Program.cs
@@ -0,0 +1,4 @@
+﻿
+namespace UnoControlsApp.Skia.Gtk
+{
+}
